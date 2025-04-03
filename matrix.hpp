3632051d988bee602eb75ae3562e92e8a6a08cc4
Mahin <mahin.r4586@gmail.com>
2025-04-02 22:31:27 -0500

    Update matrix.hpp

--- matrix.hpp
+++ matrix.hpp
@@ -1,24 +1,56 @@
 #ifndef __MATRIX_HPP__
 #define __MATRIX_HPP__
 
-#include <cstdint>
+// include vector container
 #include <vector>
 
-class Matrix {
-public:
-    Matrix(std::size_t N);
-    Matrix(std::vector<std::vector<int>> nums);
+// include input/output library
+#include <iostream>
 
-    Matrix operator+(const Matrix &rhs) const;
-    Matrix operator*(const Matrix &rhs) const;
-    void set_value(std::size_t i, std::size_t j, int n);
-    int get_value(std::size_t i, std::size_t j) const;
-    int get_size() const;
-    int sum_diagonal_major() const;
-    int sum_diagonal_minor() const;
-    void swap_rows(std::size_t r1, std::size_t r2);
-    void swap_cols(std::size_t c1, std::size_t c2);
+// include file stream library
+#include <fstream>
+
+// include string library
+#include <string>
+
+// include standard library for size_t
+#include <cstddef>
+
+class Matrix {
+private:
+    // store matrix data in 2D vector
+    std::vector<std::vector<int>> data;
+
+    // size of the matrix (N x N)
+    size_t size;
+
+public:
+    // constructor that sets matrix size
+    Matrix(size_t N);
+
+    // constructor that loads matrix from file
+    Matrix(std::ifstream &input);
+
+    // function to print matrix to console
     void print_matrix() const;
+
+    // function to add another matrix
+    Matrix add_matrix(const Matrix &other) const;
+
+    // function to multiply another matrix
+    Matrix multiply_matrix(const Matrix &other) const;
+
+    // function to return the sum of both diagonals
+    int diagonal_sum() const;
+
+    // function to swap two rows
+    void swap_rows(size_t row1, size_t row2);
+
+    // function to swap two columns
+    void swap_columns(size_t col1, size_t col2);
+
+    // function to update a specific cell
+    void update_element(size_t row, size_t col, int value);
 };
 
-#endif // __MATRIX_HPP__
+#endif
